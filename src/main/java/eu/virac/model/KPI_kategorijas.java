4e692b16cddc0868216a44f6b34efa554df3789a
Qu11k <filadelfija769@gmail.com>
2026-06-04 16:07:39 +0300

    finished category class

--- src/main/java/eu/virac/model/KPI_kategorijas.java
+++ src/main/java/eu/virac/model/KPI_kategorijas.java
@@ -1,4 +1,4 @@
-package eu.model;
+package eu.virac.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -6,6 +6,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -24,5 +27,16 @@ public class KPI_kategorijas {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Setter(value = AccessLevel.NONE)
 	private long IDCat;
+	
+	@Column(name = "Category", unique = true)
+	@NotNull
+	@NotEmpty
+	@Pattern(regexp = "[A-Ž]{1}[A-Ža-ž0-9 ]{3,40}")
+	private String Category;
+	@Column(name = "Description", unique = true)
+	@NotNull
+	@NotEmpty
+	@Pattern(regexp = "{A-Ža-ž0-9 ]{3,300}")
+	private String Description;
 
 }
