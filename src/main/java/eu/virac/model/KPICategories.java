106adcdff4418981fb1fadbc057fccc84d0a6298
Shaumis <michisillproductions@gmail.com>
2026-06-21 21:39:26 +0300

    KpiWorkDescription parametru izveide un regex noņemšana kpicategories

--- src/main/java/eu/virac/model/KPICategories.java
+++ src/main/java/eu/virac/model/KPICategories.java
@@ -1,7 +1,5 @@
 package eu.virac.model;
 
-import java.time.LocalDate;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,7 +8,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -33,12 +30,12 @@ public class KPICategories {
 	@Column(name = "Category", unique = true)
 	@NotNull
 	@NotEmpty
-	@Pattern(regexp = "[A-Ž]{1}[A-Ža-ž0-9 ]{3,40}")
 	private String Category;
+	
 	@Column(name = "Description", unique = true)
 	@NotNull
 	@NotEmpty
-	@Pattern(regexp = "{A-Ža-ž0-9 ]{3,300}")
+	
 	private String Description;
 	public KPICategories(String Category, String Description) {
 	    setCategory(Category);
--- src/main/java/eu/virac/model/KPIWorkDescription.java
+++ src/main/java/eu/virac/model/KPIWorkDescription.java
@@ -1,5 +1,48 @@
 package eu.virac.model;
 
-public class KPIWorkDescription {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name="KPI_work_description")
+public class KPIWorkDescription {
+	
+	@Column(name = "kpiwdid")
+	private long kpiwdid;
+	
+	@Column(name = "uid")
+	private long uid;
+	
+	@Column(name = "scid")
+	private long scid;
+	
+	@Column(name = "date")
+	@NotNull
+	@NotEmpty
+	private String date;
+	
+	@Column(name = "amount")
+	private int amount;
+	
+	@Column(name = "description", unique = true)
+	@NotNull
+	@NotEmpty
+	private String Description;
+	
+	public KPIWorkDescription(String date, int amount, String description) {
+	    setDate(date);
+	    setAmount(amount);
+	    setDescription(description);
+	}
 }
