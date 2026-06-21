34dceb09d6a48a2a124d72432683fda92b4ad290
Mbrokans <s25brokmart@venta.lv>
2026-06-21 19:52:09 +0300

    EDIT: added a status feature but need the ManyToOne functions

---
+++ src/main/java/eu/virac/model/KPI_Statuss.java
@@ -0,0 +1,49 @@
+package eu.virac.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Entity
+@Table(name = "KPI_Statuss")
+public class KPI_Statuss {
+
+	@Column(name = "KPI_ID")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Setter(value = AccessLevel.NONE)
+	private long kpiId;
+	
+	@Column(name = "Status")
+	@NotEmpty
+	@NotNull
+	private String status;
+
+	@Column(name = "Status_date")
+	@NotEmpty
+	@NotNull
+	private LocalDate statusDate;
+
+
+	public KPI_Statuss(String status, LocalDate statusDate) {
+		setStatus(status);
+		setStatusDate(statusDate);
+	}
+}
