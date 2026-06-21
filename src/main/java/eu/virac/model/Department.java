0e6d2dba5b48b9cff9e4146832aa281c4891f36e
Shaumis <michisillproductions@gmail.com>
2026-06-21 20:56:49 +0300

    Pēdējas izmainas

--- src/main/java/eu/virac/model/Department.java
+++ src/main/java/eu/virac/model/Department.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -23,8 +24,7 @@ public class Department {
 	
 	@Column(name = "did")
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Setter(value = AccessLevel.NONE)
+	@ManyToOne
 	private long did;
 
 	@Column(name = "description")
