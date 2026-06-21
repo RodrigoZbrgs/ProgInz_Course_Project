eb89fdbe7284034f132461efbcdfed8ff84b4cf1
Shaumis <michisillproductions@gmail.com>
2026-06-21 19:40:48 +0300

    Izveidoju user klasi un palaboju sintaksi citaas klasees

--- src/main/java/eu/virac/model/Department.java
+++ src/main/java/eu/virac/model/Department.java
@@ -8,34 +8,37 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+
 @Getter
 @Setter
 @ToString
 @Entity
-@Table(name="NodalasTable")
-public class nodala {
-	@Column(name="Idn")
-	@Id
-	@GeneratedValue (strategy= GenerationType.AUTO)
-	@Setter(value = AccessLevel.NONE)
-	private long idn;
-	@Column(name = "Description")
-	@NotEmpty
-	@NotNull
-	private String Description;
-	@Column(name = "Goals")
-	@NotEmpty
-	@NotNull
-	private String Goals;
+@Table(name = "department_table")
+
+public class Department {
 	
-	public nodala(long Idn, String Description, String Goals) {
-		setDescription(Description);
-		setGoals(Goals);
+	@Column(name = "did")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Setter(value = AccessLevel.NONE)
+	private long did;
+
+	@Column(name = "description")
+	@NotEmpty
+	@NotNull
+	private String description;
+	
+	@Column(name = "goals")
+	@NotEmpty
+	@NotNull
+	private String goals;
+
+	public Department(long did, String description, String goals) {
+		setDescription(description);
+		setGoals(goals);
 	}
 }
---
+++ src/main/java/eu/virac/model/Users.java
@@ -0,0 +1,66 @@
+package eu.virac.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Table(name = "users_table")
+@Entity
+public class Users {
+
+	@Column(name = "uid")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Setter(value = AccessLevel.NONE)
+	private long uid;
+
+	@Column(name = "name")
+	@NotNull
+	@NotEmpty
+	@Pattern(regexp = "[A-Ž]{1}[a-ž]{2,20}([ ]{1}([A-Ž]{1}[a-ž]{2,20}))?")
+	private String name;
+
+	@Column(name = "surname")
+	@NotNull
+	@NotEmpty
+	@Pattern(regexp = "[A-Ž]{1}[a-ž]{2,20}([ ]{1}([A-Ž]{1}[a-ž]{2,20}))?")
+	private String surname;
+
+	@Column(name = "email")
+	@NotNull
+	@NotEmpty
+	private String email;
+
+	@Column(name = "password")
+	@NotNull
+	@NotEmpty
+	private String password;
+
+	@Column(name = "section")
+	@NotNull
+	@NotEmpty
+	private Department department;
+
+	public Users(String name, String surname, String email, String password, Department department) {
+		setName(name);
+		setSurname(surname);
+		setEmail(email);
+		setPassword(password);
+		setDepartment(department);
+	}
+}
