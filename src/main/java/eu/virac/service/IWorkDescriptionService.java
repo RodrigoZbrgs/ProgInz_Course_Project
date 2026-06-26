08f61e9fe294b6b293613def75d091e93d481446
Qu11k <filadelfija769@gmail.com>
2026-06-26 22:02:25 +0300

    after saves commit (safety)

--- src/main/java/eu/virac/service/IWorkDescriptionService.java
+++ src/main/java/eu/virac/service/IWorkDescriptionService.java
@@ -5,8 +5,12 @@ import eu.virac.model.KPIWorkDescription;
 
 public interface IWorkDescriptionService {
 	 public abstract ArrayList<KPIWorkDescription> findAllWorkDescriptions();
+	 
 	    public abstract Optional<KPIWorkDescription> findWorkDescriptionById(long id);
+	    
 	    public abstract KPIWorkDescription saveWorkDescription(KPIWorkDescription workDescription);
+	    
 	    public abstract KPIWorkDescription updateWorkDescription(long id, KPIWorkDescription workDescription);
+	    
 	    public abstract void deleteWorkDescription(long id);
 }
--- src/main/java/eu/virac/service/IWorkDescriptionsFilter.java
+++ src/main/java/eu/virac/service/IWorkDescriptionsFilter.java
@@ -7,7 +7,9 @@ import eu.virac.model.KPIWorkDescription;
 
 public interface IWorkDescriptionsFilter {
 	public abstract ArrayList<KPIWorkDescription> filterByDescriptionContaining(String keyword);
+	
     public abstract ArrayList<KPIWorkDescription> filterByDate(LocalDate date);
+    
     public abstract ArrayList<KPIWorkDescription> filterByAmountGreaterThan(int amount);
 }
 
